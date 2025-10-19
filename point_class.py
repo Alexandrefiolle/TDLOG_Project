01f04e03c3d696e2c0a154608b5c3346abcd253c
Alexandre fiolle <alexandre.fiolle@gmail.com>
2025-10-19 10:07:55 +0200

    class Point started

---
+++ point_class.py
@@ -0,0 +1,25 @@
+import numpy as np
+from __future__ import annotations
+
+class Point():
+    def __init__(self, x: int, y: int) -> None:
+        self._x = x
+        self._y = y
+
+    @property
+    def x(self) -> int:
+        return self._x
+    @x.setter
+    def x(self, new_x: int) -> None:
+        self._x = new_x
+    
+    @property
+    def y(self) -> int:
+        return self._y
+    @y.setter
+    def y(self, new_y) -> None:
+        self._y = new_y
+
+    def norm(self, other: Point) -> float:
+        return np.sqrt((self._x-other._x)**2 + (self._y-other._y)**2)
+
