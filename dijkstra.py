094d95f190c025eaa129586fe7edda9841b1e8e5
Léo Garnaud <leo.garnaud@eleves.enpc.fr>
2025-11-03 14:47:13 +0100

    justification os the comparison between two points

--- dijkstra.py
+++ dijkstra.py
@@ -6,40 +6,40 @@ import heapq
 
 epsilon = 2.0
 
-class PriorityQueue:
-    def __init__(self, dic: dict[pc.Point, float]) -> None:
-        self._dic = dic
-    
-    def _find_higher_priority_point(self) -> pc.Point: # ensuite, utiliser un tas de priorité
-        dist_inf = np.inf
-        best_point = None
-        for (point, distance) in self._dic.items():
-            if dist_inf > distance:
-                dist_inf = distance
-                best_point = point
-        return best_point
-
-    def append(self, point: pc.Point, priority: float) -> None:
-        self._dic[point] = priority
-
-    def remove(self) -> pc.Point:
-        best_point = self._find_higher_priority_point()
-        del self._dic[best_point] 
-        return best_point
-    
-    def size(self) -> int:
-        return len(self._dic)
+#class PriorityQueue:
+#    def __init__(self, dic: dict[pc.Point, float]) -> None:
+#        self._dic = dic
+#    
+#    def _find_higher_priority_point(self) -> pc.Point:
+#        dist_inf = np.inf
+#        best_point = None
+#        for (point, distance) in self._dic.items():
+#            if dist_inf > distance:
+#                dist_inf = distance
+#                best_point = point
+#        return best_point
+#
+#    def append(self, point: pc.Point, priority: float) -> None:
+#        self._dic[point] = priority
+#
+#    def remove(self) -> pc.Point:
+#        best_point = self._find_higher_priority_point()
+#        del self._dic[best_point] 
+#        return best_point
+#    
+#    def size(self) -> int:
+#        return len(self._dic)
 
 class PriorityQueue_heap:
     def __init__(self, heap: list[pc.Point, float]) -> None:
         self._heap = heap
     
-    def _find_higher_priority_point(self) -> pc.Point: # ensuite, utiliser un tas de priorité
+    def _find_higher_priority_point(self) -> pc.Point: 
         best_point = self._heap[0][1]
         return best_point
 
     def append(self, point: pc.Point, priority: float) -> None:
-        heapq.heappush(self._heap, (priority, point))
+        heapq.heappush(self._heap, (priority, point)) # this function adds the new value (priority, point) by preseving the heap structure
 
     def remove(self) -> pc.Point:
         best_point = self._find_higher_priority_point()
@@ -55,9 +55,7 @@ def distances_costs(start: pc.Point, end: pc.Point, grey_levels: ui.GreyImage) -
     for point in grey_levels.graph.keys():
         dist[point] = np.inf
     dist[start] = 0
-    #to_visit = PriorityQueue({})
     to_visit = PriorityQueue_heap([])
-    #to_visit.append(start, 0)
     to_visit.append(start, 0)
     while to_visit.size() > 0:
         candidate = to_visit.remove()
