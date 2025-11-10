01afcae548f6d4256aec90128771aae51919b54e
Alexandre fiolle <alexandre.fiolle@gmail.com>
2025-11-10 15:07:06 +0100

    send image to class Vue

--- interface.py
+++ interface.py
@@ -7,31 +7,42 @@ import PyQt6.QtWidgets as widgets
 import PyQt6.QtGui as gui
 from PIL import Image
 
+class Vue(widgets.QGroupBox):
+    pass
+    def __init__(self):
+        super().__init__(None)
+        self.texte = widgets.QLabel("Lorem ipsum &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&")
+        self.texte.setVisible(True)
+        self.image = gui.QImage()
+    
+    def change_image(self, path) -> None:
+        pass
+
 class Menu(widgets.QGroupBox):
      
-    def __init__(self) -> None:
+    def __init__(self, vue: Vue) -> None:
         super().__init__(None)
-        assert isinstance(self, widgets.QWidget), "problem"
-
         self.select_button = widgets.QPushButton("Select an image", self)
+        self.select_button.setGeometry(10, 10, 150, 30)
         self.select_button.clicked.connect(self.select_button_was_clicked)
         self.original_image_button = widgets.QPushButton("Original image", self)
+        self.original_image_button.setGeometry(10, 50, 150, 30)
         self.original_image_button.clicked.connect(self.original_image_button_was_selected)
         self.distances_map_button = widgets.QPushButton("Distances map", self)
+        self.distances_map_button.setGeometry(10, 90, 150, 30)
         self.distances_map_button.clicked.connect(self.distances_map_button_was_selected)
         self.gradients_map_button = widgets.QPushButton("Gradients map", self)
+        self.gradients_map_button.setGeometry(10, 130, 150, 30)
         self.gradients_map_button.clicked.connect(self.gradients_map_button_was_clicked)
         self.path_button = widgets.QPushButton("Print the optimal path", self)
+        self.path_button.setGeometry(10, 170, 150, 30)
         self.path_button.clicked.connect(self.path_button_was_clicked)
+        self._vue = vue
 
     def select_button_was_clicked(self) -> None:
         """Handles the button click event to open a file dialog and display the selected image."""
-        pass
-        #file_name, _ = widgets.QFileDialog.getOpenFileName(self)
-        #pic = widgets.QLabel(self)
-        #pic.setGeometry(0, 50, 1325, 701)
-        #pic.setPixmap(PyQt6.QtGui.QPixmap(file_name).scaledToHeight(500, mode = Qt.TransformationMode.SmoothTransformation))
-        #pic.show()
+        file_name, _ = widgets.QFileDialog.getOpenFileName(self)
+        self._vue.change_image(file_name)
 
     def original_image_button_was_selected(self) -> None:
         pass
@@ -44,15 +55,6 @@ class Menu(widgets.QGroupBox):
     
     def path_button_was_clicked(self) -> None:
         pass
-
-class Vue(widgets.QGroupBox):
-    pass
-    def __init__(self):
-        super().__init__(None)
-        self.texte = widgets.QLabel("Lorem ipsum &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&")
-        self.texte.setVisible(True)
-        self.image = gui.QImage()
-        
 
 class Window(widgets.QMainWindow):
     """A simple window class to open and display an image."""
