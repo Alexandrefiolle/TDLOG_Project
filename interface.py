c9b9b8c6362b4cc705a7fb0ff8be10ba81647c35
Alexandre fiolle <alexandre.fiolle@gmail.com>
2025-11-15 17:32:10 +0100

    Interface improved with the access to the distances map

--- interface.py
+++ interface.py
@@ -1,11 +1,15 @@
 "This file aims at creating a simple interface which allows the user to open and display an image."
 
+from __future__ import annotations 
 import sys
 import PyQt6
 from PyQt6.QtCore import Qt
 import PyQt6.QtWidgets as widgets
 import PyQt6.QtGui as gui
 from PIL import Image
+import dijkstra
+import point_class as pc
+import manipulation as ui
 
 class Vue(widgets.QGroupBox):
     """
@@ -25,7 +29,11 @@ class Vue(widgets.QGroupBox):
     def change_image(self, path) -> None:
         """Changes the displayed image to the one located at the given path."""
         self.image.setPixmap(gui.QPixmap(path).scaledToWidth(1000, mode = Qt.TransformationMode.SmoothTransformation))
-
+    
+    def print_stocked_image(self, image_name: str) -> None:
+        """Displays the image currently stored in the view."""
+        self.image.setPixmap(gui.QPixmap(image_name).scaledToWidth(1000, mode = Qt.TransformationMode.SmoothTransformation))
+        
 class Menu(widgets.QGroupBox):
     """
     A menu class with buttons to interact with the image view,
@@ -50,22 +58,42 @@ class Menu(widgets.QGroupBox):
         self.path_button = widgets.QPushButton("Print the optimal path", self)
         self.path_button.setGeometry(10, 170, 150, 30)
         self.path_button.clicked.connect(self.path_button_was_clicked)
-        self._original_vue = vue
-        self._distances_map_vue = None
-        self._gradients_map_vue = None
+        self._vue = vue
+        self._original_image_name = 'Carte.png'
+        self._distances_map_image_name = 'distances_map.png'
+        self._distances_map_computed = False
+        self._gradients_map_image_name = 'gradients_map.png'
+        self._gradients_map_computed = False
 
     def select_button_was_clicked(self) -> None:
         """Handles the button click event to open a file dialog and display the selected image."""
         file_name, _ = widgets.QFileDialog.getOpenFileName(self)
-        self._original_vue.change_image(file_name)
+        self._vue.change_image(file_name)
+        self._original_image_name = file_name
+        self._distances_map_computed = False
+        self._gradients_map_computed = False
 
     def original_image_button_was_selected(self) -> None:
         """Handles the button click event to display the original image."""
-        pass
+        self._vue.print_stocked_image(self._original_image_name)
+
+    def distances_map_creation(self, start: pc.Point, end: pc.Point) -> None:
+        """Creates the distances map and stores it in the corresponding view."""
+        im = ui.GreyImage(self._original_image_name)
+        distances_map_image = dijkstra.distances_map(start, end, im)
+        img = Image.fromarray(distances_map_image)
+        img.save(self._distances_map_image_name)
+        self._distances_map_computed = True
 
     def distances_map_button_was_selected(self) -> None:
         """Handles the button click event to display the distances map."""
-        pass
+        if not self._distances_map_computed:
+            start = pc.Point(10,10)
+            end = pc.Point(400,400)
+            self.distances_map_creation(start, end)
+            self._vue.print_stocked_image(self._distances_map_image_name)
+        else:
+            self._vue.print_stocked_image(self._distances_map_image_name)
 
     def gradients_map_button_was_clicked(self) -> None:
         """Handles the button click event to display the gradients map."""
