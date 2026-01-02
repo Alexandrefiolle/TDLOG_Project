8e95d936a421bc79cfbde2f3322d2b8b76e07437
Léo Garnaud <leo.garnaud@eleves.enpc.fr>
2026-01-02 22:42:45 +0100

    file segmentation

---
+++ segmentation.py
@@ -0,0 +1,60 @@
+import point_class as pc
+import manipulation as ui
+from collections import deque
+import numpy as np
+import heapq
+import matplotlib.pyplot as plt
+import matplotlib.colors as col
+from math import*
+import random
+import time
+import interface as vis
+import dijkstra as d
+
+def points(nb_points: int, height: int, width: int) -> list[pc.Point]:
+    list_points = []
+    for _ in range(nb_points):
+        x = random(0, width)
+        y = random(0, height)
+        p = pc.Point(x, y)
+        list_points.append(p)
+    return list_points
+
+def distances_costs(start: pc.Point, grey_levels: ui.GreyImage, list_visited: list[pc.Point]) -> dict[pc.Point, float]:
+    """Computes the list of shortest path costs from start until we reach the end point"""
+    dist = {}
+    for point in grey_levels.graph.keys():
+        dist[point] = np.inf
+    dist[start] = 0
+    to_visit = d.PriorityQueue_heap([])
+    to_visit.append(start, 0)
+    while to_visit.size() > 0:
+        candidate = to_visit.remove()
+        list_visited.append(candidate)
+        for neighbor in grey_levels.neighbors(candidate):
+            assert neighbor.x < grey_levels.width and neighbor.y < grey_levels.height
+            cost = grey_levels.cost(start, neighbor, d.epsilon)
+            if dist[neighbor] > dist[candidate] + cost:
+                dist[neighbor] = dist[candidate] + cost
+                to_visit.append(neighbor, dist[neighbor])
+    return dist
+
+def distances_map(list_point: list[pc.Points], im: ui.GreyImage) -> list[np.ndarray]:
+    list_distance_map = []
+    for k in range(len(list_point)):
+        dist = distances_costs(list_point[k], im, [])
+        dist_map = d.coloration_map(dist, im)
+        list_distance_map.append[dist_map]
+    return list_distance_map
+
+if __name__ == "__main__":
+    im = ui.GreyImage('EZEZEZEZ.png')
+    #im = ui.GreyImage('Carte.png')
+    r = random(1,10)
+    list_point = points(r, im.height, im.width)
+    list_distance_map = distances_map(list_point, im)
+    for k in range(r):
+        img = ui.Image.fromarray(list_distance_map[k], 'RGB')
+        img.show()
+
+        
